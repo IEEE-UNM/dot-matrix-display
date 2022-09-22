f2be63b6d373d8c28576e8ea546ba336d2651f4d
Ushan Fernando <ushanfernando123@gmail.com>
2022-09-22 13:36:05 +0800

    Working code for scrolling sign

--- dot-matrix-display.ino
+++ dot-matrix-display.ino
@@ -1,22 +1,26 @@
-#include <MD_MAX72xx.h>
 #include <MD_Parola.h>
+#include <MD_MAX72xx.h>
 #include <SPI.h>
 
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
-#define CS_PIN 3
 
-MD_Parola display(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+#define CLK_PIN   13
+#define DATA_PIN  11
+#define CS_PIN    10
 
-void setup() {
-  display.begin();
-  display.setIntensity(0);
-  display.displayClear();
-  display.displayScroll("IEEE UNM", PA_CENTER, PA_SCROLL_LEFT, 100);
+MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+
+void setup(void)
+{
+  P.begin();
+  P.setSpeed(60);
+  P.setPause(0);
+  P.setIntensity(0);
 }
 
-void loop() {
-  if (display.displayAnimate()) {
-    display.displayReset();
-  }
+void loop(void)
+{
+  if (P.displayAnimate())
+    P.displayText("IEEE UNM Student Branch", PA_CENTER, P.getSpeed(), P.getPause(), PA_SCROLL_LEFT, PA_SCROLL_LEFT);
 }
